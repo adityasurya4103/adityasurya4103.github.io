42434e0d5198464a8244ef278699e4ebb95530eb
Aditya Suryawanshi <97177344+adityasurya4103@users.noreply.github.com>
2023-09-17 18:46:23 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,39 @@
+function Toggle() {
+    const navbar = document.getElementById("nav");
+    // navbar.style.display="block";
+    console.log('clicked');
+    if (navbar.style.display === "block") {
+        navbar.style.display = "none";
+    }
+    else {
+        navbar.style.display = "block";
+    }
+
+}
+
+
+var swiper = new Swiper(".mySwiper", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+      rotate: 0,
+      stretch: 0,
+      depth: 100,
+      modifier: 2,
+      slideShadows: true,
+    },
+    loop: true,
+    autoplay: {
+        delay: 3000, 
+        disableOnInteraction: false, 
+    },
+});
+
+
+// function flipCard(card) {
+//     card.classList.toggle('flipped');
+// }
+
+
